893c47a6c4c888df7caca35cbb97d2e3cda15238
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-04-22 20:13:05 -0300

    Feat. Converting duration of videos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 }
 
 type HomeProps = {
@@ -15,6 +24,7 @@ export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
+      <p>{ JSON.stringify(props.episodes) }</p>
     </>
   )
 }
@@ -28,9 +38,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+
   return{
     props:{
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8
   }
